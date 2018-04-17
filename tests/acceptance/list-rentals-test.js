13a0e5728ccb9f24d877796c94294d064719c4c5
Alex Bennett <benedictus.ab@gmail.com>
2018-04-17 14:48:47 -0600

    added rentals/styling/addons

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+
+module('Acceptance | my acceptance test', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+  });
+});
